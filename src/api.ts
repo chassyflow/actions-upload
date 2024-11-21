8675ae1ff8fb6eb2cc530110598ec756078daca2
jackjohn7 <jack@chassy.io>
2024-11-21 10:42:16 -0600

    removed TODO comment

--- src/api.ts
+++ src/api.ts
@@ -7,16 +7,16 @@ export type Upload = {
   uploadURI: string
 }
 
-// TODO: Add better types for image and package
-
 export type CreateImage = Upload & { image: Image }
 
 export type CreatePackage = Upload & { package: Package }
 
 export type Image = {
   id: string
+  // other properties exist but do not matter here
 }
 
 export type Package = {
   id: string
+  // other properties exist but do not matter here
 }
